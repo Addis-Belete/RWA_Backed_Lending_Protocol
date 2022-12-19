e00c5544730ce1c3c687bf409e6612d74a2accb3
Addis Belete <addis.bel@gmail.com>
2022-12-19 19:50:44 +0300

    Add burnRWA function

--- src/core/NFT.sol
+++ src/core/NFT.sol
@@ -32,6 +32,12 @@ contract NFT is ERC721URIStorage {
      */
     event RWAMinted(address indexed owner, uint256 value, AssetType indexed assetType, string _tokenURI);
 
+    /**
+     * @notice Emitted after RWA burned or removed
+     * @param _tokenId The Id of the token(RWA) burned
+     */
+    event RWABurned(uint256 indexed _tokenId);
+
     constructor(address _admin) ERC721("Real World Asseet", "RWA") {
         admin = _admin;
         //This constructor changes to initialize for upgradable contract
@@ -39,6 +45,11 @@ contract NFT is ERC721URIStorage {
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only called by admin");
+        _;
+    }
+
+    modifier checkAddress(address _address) {
+        require(_address != address(0), "Invalid address");
         _;
     }
 
@@ -53,12 +64,23 @@ contract NFT is ERC721URIStorage {
     function mintRWA(address _owner, uint256 _value, AssetType _assetType, string calldata _tokenURI)
         external
         onlyAdmin
+        checkAddress(_owner)
     {
         tokenId++;
         realWorldAssetDetails[tokenId] = RWA(_assetType, _value, block.timestamp, _owner);
         _mint(_owner, tokenId);
         _setTokenURI(tokenId, _tokenURI);
-
         emit RWAMinted(_owner, _value, _assetType, _tokenURI);
+    }
+
+    /**
+     * @notice Removes or burns Asset
+     * @dev Only called by the admin
+     * @param _tokenId The Id of the token to be removed
+     */
+    function burnRWA(uint256 _tokenId) external onlyAdmin {
+        _burn(tokenId);
+        delete realWorldAssetDetails[tokenId];
+        emit RWABurned(_tokenId);
     }
 }
