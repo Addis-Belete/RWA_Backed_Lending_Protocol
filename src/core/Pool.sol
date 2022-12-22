1a612fc61a2818f370c20a3650f8e9c2d75a9e78
Addis Belete <addis.bel@gmail.com>
2022-12-22 16:34:38 +0300

    Import ERC4626.sol

--- src/core/Pool.sol
+++ src/core/Pool.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.13;
 
-contract Pool {}
+contract Pool {
+    /**
+     * Main functions
+     */
+
+    /**
+     * @notice Used by liquidity providers to deposit stable coins as a liqudity
+     */
+    function deposit() external {}
+
+    function withdraw() external {}
+
+    function borrow() external {}
+
+    function repay() external {}
+}
