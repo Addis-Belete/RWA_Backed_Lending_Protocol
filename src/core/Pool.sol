f289790a03665ef736e3003ed535d0d9135f9f6d
Addis Belete <addis.bel@gmail.com>
2022-12-30 17:39:39 +0300

    Add deposit functionality

--- src/core/Pool.sol
+++ src/core/Pool.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.13;
 
 import "../Interfaces/IReceiptToken.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 contract Pool {
     /**
@@ -19,16 +20,47 @@ contract Pool {
      * @notice Loan repayment can be extended but interest increase by 2% every year for additional three years
      */
 
-    IReceiptToken internal receiptToken;
+    struct UserInfo {
+        uint256 depositStartTime;
+        bool isLiqiudator;
+        uint256 interestAccumulated;
+    }
 
-    constructor(address _receiptTokenAddress) {
+    mapping(address => UserInfo) internal userInfo;
+    IReceiptToken internal receiptToken;
+    address internal underlying;
+
+    event AssetDeposited(address from, uint256 amount);
+
+    modifier checkAddress(address _address) {
+        require(_address != address(0), "Invalid address");
+        _;
+    }
+
+    constructor(address _receiptTokenAddress, address _underlying) {
         receiptToken = IReceiptToken(_receiptTokenAddress);
+        underlying = _underlying;
     }
 
     /**
      * @notice Used to deposit Assets to the pool and get rewards
+     * Everytime user deposited The initail balance interest calculated and stored.
      */
-    function deposit(uint256 assets, address receiver) external {}
+    function deposit(uint256 amount, address from) external checkAddress(from) {
+        require(IERC20(underlying).transferFrom(from, address(this), amount), "Transfer failed");
+        if (userInfo[from].isLiqiudator) {
+            uint256 _accumulatedInterest = calculateInterest();
+            UserInfo storage _userInfo = userInfo[from];
+            _userInfo.depositStartTime = block.timestamp;
+            _userInfo.interestAccumulated += _accumulatedInterest;
+        } else {
+            UserInfo memory _userInfo = UserInfo(block.timestamp, true, 0);
+            userInfo[from] = _userInfo;
+        }
+
+        IReceiptToken.mint(from, amount);
+        emit AssetDeposited(from, amount);
+    }
 
     function depositWithPermint(uint256 assets, address receiver) external {}
     /**
@@ -51,4 +83,10 @@ contract Pool {
      */
 
     function repayWithPermit() external {}
+
+    function claimInterest() external {}
+
+    function checkYourInterest() external {}
+
+    function calculateInterest() private returns (uint256) {}
 }
